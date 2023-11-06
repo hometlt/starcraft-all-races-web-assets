fce2316bbcfb1a483af6478cde94caa79b80b25d
Denis Ponomarev <visceroi@gmail.com>
2023-11-06 23:14:08 +0100

    icons

---
+++ icons/rename.mjs
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+import url from 'url'
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+let files = fs.readdirSync('./icons')
+for (let file of files) {
+  let filenamefull =path.resolve(__dirname +'/' + file)
+  let isdir = fs.lstatSync(filenamefull).isDirectory()
+  if(!isdir){
+
+    let dest = path.resolve(__dirname +'/' +  file.toLowerCase())
+    if(filenamefull !== dest){
+      fs.rename(filenamefull,dest, function(err) {
+        if ( err ) console.log('ERROR: ' + err);
+      });
+    }
+
+  }
+}
